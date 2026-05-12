ff211819f1896c6e4a679560b8cf4733491f670c
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-05-12 13:20:47 +0200

    fix: remove :: prefix from new & delete operators so it compiles

--- h/syscall_cpp.hpp
+++ h/syscall_cpp.hpp
@@ -2,6 +2,6 @@
 
 #include "syscall_c.h"
 
-void* ::operator new(size_t);
+void* operator new(size_t);
 
-void ::operator delete(void*);
+void operator delete(void*) noexcept;
--- src/syscall_cpp.cpp
+++ src/syscall_cpp.cpp
@@ -1,4 +1,4 @@
 #include "../h/syscall_cpp.hpp"
 
-void* ::operator new(size_t size) { return mem_alloc(size); }
-void ::operator delete(void* ptr) { mem_free(ptr); }
+void* operator new(size_t size) { return mem_alloc(size); }
+void operator delete(void* ptr) noexcept { mem_free(ptr); }
